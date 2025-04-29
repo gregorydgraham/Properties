3c5190645266aa14dc99d31b08db1899fe8df373
Gregory Graham <gregory@gregs.co.nz>
2025-04-29 12:19:57 +1200

    Improved JavaDocs now that JavaDocs are being checked

--- src/main/java/nz/co/gregs/properties/PropertyContainerWrapperFactory.java
+++ src/main/java/nz/co/gregs/properties/PropertyContainerWrapperFactory.java
@@ -5,11 +5,9 @@ import java.util.Map;
 import nz.co.gregs.properties.adapt.PropertyTypeHandler;
 
 /**
- * Constructs class adaptors for property containers classes and maintains an in-memory
- * cache for re-use. Creating class adaptors is expensive and this class is
- * provided as a convenience for anything that needs to access class adaptors
- * for multiple types and would benefit from the performance improvement of
- * caching their values.
+ * Constructs class adaptors for property containers classes and maintains an in-memory cache for re-use. Creating class adaptors is expensive and this class is
+ * provided as a convenience for anything that needs to access class adaptors for multiple types and would benefit from the performance improvement of caching
+ * their values.
  *
  * <p>
  * Note that class adaptors are immutable, so this is safe to do.
@@ -21,45 +19,42 @@ import nz.co.gregs.properties.adapt.PropertyTypeHandler;
  */
 public class PropertyContainerWrapperFactory {
 
-    /**
-     * Thread-safety: access to this object must be synchronized on it
-     */
-    private final Map<Class<?>, PropertyContainerClass> classWrappersByClass = new HashMap<Class<?>, PropertyContainerClass>();
-	private final PropertyTypeHandler handler;
-		
-		
-		public PropertyContainerWrapperFactory(PropertyTypeHandler handler){
-			this.handler = handler;
-		}
+  /**
+   * Thread-safety: access to this object must be synchronized on it
+   */
+  private final Map<Class<?>, PropertyContainerClass> classWrappersByClass = new HashMap<Class<?>, PropertyContainerClass>();
+  private final PropertyTypeHandler handler;
 
-    /**
-     * Gets the class adaptor for the given class. If an adaptor for the given
-     * class has not yet been created, one will be created and added to the
-     * internal cache.
-     *
-	 * @param clazz clazz
-     * @return the class adaptor
-     */
-    public PropertyContainerClass classWrapperFor(Class<? extends PropertyContainer> clazz) {
-        synchronized (classWrappersByClass) {
-            PropertyContainerClass wrapper = classWrappersByClass.get(clazz);
-            if (wrapper == null) {
-                wrapper =new PropertyContainerClass(clazz, handler);
-                classWrappersByClass.put(clazz, wrapper);
-            }
-            return wrapper;
-        }
-    }
+  public PropertyContainerWrapperFactory(PropertyTypeHandler handler) {
+    this.handler = handler;
+  }
 
-    /**
-     * Gets the object adaptor for the given object.If an adaptor for the
- object's class has not yet been created, one will be created and added to
- the internal cache.
-     *
-     * @param object the DBRow instance to wrap
-     * @return the object adaptor for the given object
-     */
-    public PropertyContainerWrapper instanceWrapperFor(PropertyContainer object) {
-        return classWrapperFor(object.getClass()).instanceWrapperFor(object);
+  /**
+   * Gets the class adaptor for the given class. If an adaptor for the given class has not yet been created, one will be created and added to the internal
+   * cache.
+   *
+   * @param clazz clazz
+   * @return the class adaptor
+   */
+  public PropertyContainerClass classWrapperFor(Class<? extends PropertyContainer> clazz) {
+    synchronized (classWrappersByClass) {
+      PropertyContainerClass wrapper = classWrappersByClass.get(clazz);
+      if (wrapper == null) {
+        wrapper = new PropertyContainerClass(clazz, handler);
+        classWrappersByClass.put(clazz, wrapper);
+      }
+      return wrapper;
     }
+  }
+
+  /**
+   * Gets the object adaptor for the given object.If an adaptor for the object's class has not yet been created, one will be created and added to the internal
+   * cache.
+   *
+   * @param object the DBRow instance to wrap
+   * @return the object adaptor for the given object
+   */
+  public PropertyContainerWrapper instanceWrapperFor(PropertyContainer object) {
+    return classWrapperFor(object.getClass()).instanceWrapperFor(object);
+  }
 }
