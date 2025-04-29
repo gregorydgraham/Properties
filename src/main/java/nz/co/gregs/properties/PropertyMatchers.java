6b657d5b7bb76c6e4bdbd693699a455174322dd9
Gregory Graham <gregory@gregs.co.nz>
2025-04-29 14:17:58 +1200

    Improved JavaDocs now that JavaDocs are being checked

--- src/main/java/nz/co/gregs/properties/PropertyMatchers.java
+++ src/main/java/nz/co/gregs/properties/PropertyMatchers.java
@@ -11,26 +11,48 @@ import org.hamcrest.StringDescription;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.hamcrest.TypeSafeMatcher;
 
+/**
+ * Useful matchers for finding the correct properties.
+ *
+ * @author Malcolm Lett
+ * @author Gregory Graham
+ */
 public class PropertyMatchers {
+
+  /**
+   * default constructor.
+   *
+   */
+  public PropertyMatchers() {
+  }
 	
-	public static Matcher<String> matchesRegex(final String regex) {
+
+	/**
+	 * Creates a regex matcher that avoids null pointer exceptions.
+   * 
+   * @param regex a regular expression
+	 * @return a matcher that will use the regular expression to match strings after checking they're not null
+	 */
+  public static Matcher<String> matchesRegex(final String regex) {
 		return new TypeSafeMatcher<String>() {
+      @Override
 			public void describeTo(Description description) {
-				description.appendText("matches ").appendValue(regex);
+				description.appendText("matches ").appendValue((regex==null?"[NULL]":regex));
 			}
 
 			@Override
 			protected boolean matchesSafely(String item) {
-				return (item != null) && item.matches(regex);
+				return (regex != null) && (item != null) && item.matches(regex);
 			}
 		};
 	}
 	
 	/**
 	 * Gets the first found item where there are many.
-	 * @param c
-	 * @param matcher
-	 * @return
+   * @param <E> a super class of all members 
+	 * @param c a collection of items to check
+	 * @param matcher the matcher to test the items with
+	 * @return the first item to successfully match with the matcher
 	 */
 	public static <E> E firstItemOf(Collection<E> c, Matcher<? super E> matcher) {
 		for (E item: c) {
@@ -43,8 +65,9 @@ public class PropertyMatchers {
 	
 	/**
 	 * Gets the zero or one item accepted by the matcher.
-	 * @param c
-	 * @param matcher
+   * @param <E> a super class of all members 
+	 * @param c a collection of items to check
+	 * @param matcher the matcher to test the items with
 	 * @return the item or null if not found
 	 * @throws AssertionError if multiple items match
 	 */
@@ -72,6 +95,9 @@ public class PropertyMatchers {
 	 * <p>
 	 * For example:  itemOf(collection, hasName(smelly))
 	 *          vs.  itemOf(collection, that(hasName(smelly)))
+   * @param <T> the class of the return type
+   * @param matcher matcher to decorate
+   * @return a new matcher containing the decorated matcher
 	 */
 	public static <T> Matcher<T> that(final Matcher<T> matcher) {
 		return new BaseMatcher<T>() {
@@ -93,7 +119,13 @@ public class PropertyMatchers {
 		};
 	}
 	
-	public static Matcher<JavaProperty> hasJavaPropertyName(final String name) {
+  /**
+   * A matcher for finding properties based on the property name.
+   *
+   * @param name the property name to find
+   * @return a matcher
+   */
+  public static Matcher<JavaProperty> hasJavaPropertyName(final String name) {
 		return new TypeSafeDiagnosingMatcher<JavaProperty>() {
 			@Override
 			public void describeTo(Description description) {
@@ -111,6 +143,12 @@ public class PropertyMatchers {
 		};
 	}
 
+	
+  /**
+   * A matcher for finding properties based on the whether it is a field property or bean property.
+   *
+   * @return a matcher
+   */
 	public static Matcher<JavaProperty> isJavaPropertyField() {
 		return new TypeSafeDiagnosingMatcher<JavaProperty>() {
 			@Override
